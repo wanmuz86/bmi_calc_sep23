46ed98f909efd8d552333d0152829e6176ebeafc
Wan Muzaffar <Wan Muzaffar>
2023-09-14 10:12:24 +0800

    Additional code

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -35,71 +35,97 @@ class _HomePageState extends State<HomePage> {
   double _heightSliderValue = 170; // private property
   double _weightSliderValue = 70; // Declaring with data type -> explicit declaration
   var _bmi = 0; // declaring var -> implicit declaration
+  var _message = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BMI Calculator"),
+        title: const Text("BMI Calculator"),
       ),
-      body: Center(
-          child: Column(
-        children: [
-          Text("BMI Calculator", style: TextStyle(fontSize: 32, color: Colors.red),),
-          SizedBox(height: 8,),
-          Text("We care about your health", style: TextStyle(fontSize: 24),),
-          SizedBox(height: 8,),
-          Image.asset("assets/bmi_calc.jpg"),
-          SizedBox(height: 8,),
-          Text("Height (${_heightSliderValue.round()} cm)", style: TextStyle(fontSize: 24),),
-          SizedBox(height: 8,),
-          // Slider
-          Slider(
-            value: _heightSliderValue,
-            min:50,
-            max: 200,
-            onChanged: (double value) {
-              setState(() {
-                _heightSliderValue = value;
-              });
-            },
-          ),
-          Text("Weight (${_weightSliderValue.round()} kg)",
-            style: TextStyle(fontSize: 24),),
-          SizedBox(height: 8,),
-          // Slider
-          Slider(
-            value: _weightSliderValue,
-            max: 180,
-            min:40,
-            onChanged: (double value) {
-              setState(() {
-                _weightSliderValue = value;
-              });
-            },
-          ),
-          SizedBox(height: 8,),
-          Text("Your BMI is $_bmi"), // $ and ${ } is to bring out variable
-          TextButton.icon(
-              onPressed: () {
-                // ( weight in kg / (height in metre)^2) = bmi
-
-                var bmiTemp = _weightSliderValue / pow((_heightSliderValue / 100),2);
-                print(bmiTemp);
-                // ONLY Assigning bmiTemp.round() into _bmi variable
-               // _bmi = bmiTemp.round();
-
-                // Assigning  bmiTemp.round() into _bmi variable
-                // The widget to be refreshed
-
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+          children: [
+            const Text("BMI Calculator", style: TextStyle(fontSize: 32, color: Colors.red),),
+            const SizedBox(height: 8,),
+            const Text("We care about your health", style: TextStyle(fontSize: 24),),
+            const SizedBox(height: 8,),
+            Image.asset("assets/bmi_calc.jpg"),
+            const SizedBox(height: 8,),
+            Text("Height (${_heightSliderValue.round()} cm)", style: const TextStyle(fontSize: 24),),
+            const SizedBox(height: 8,),
+            // Slider
+            Slider(
+              value: _heightSliderValue,
+              min:50,
+              max: 200,
+              onChanged: (double value) {
                 setState(() {
-                  _bmi = bmiTemp.round();
+                  _heightSliderValue = value;
                 });
-
               },
-              icon: Icon(Icons.favorite),
-              label: Text("Calculate"))
-        ],
-      )),
+            ),
+            Text("Weight ${_weightSliderValue.round()} kg)",
+              style: const TextStyle(fontSize: 24),),
+            const SizedBox(height: 8,),
+            // Slider
+            Slider(
+              value: _weightSliderValue,
+              max: 180,
+              min:40,
+              onChanged: (double value) {
+                setState(() {
+                  _weightSliderValue = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8,),
+            _bmi != 0 ? Text("Your BMI is $_bmi") : const SizedBox(),
+            const SizedBox(height: 8,),
+            Text(_message),// $ and ${ } is to bring out variable
+            const SizedBox(height: 8,),
+            TextButton.icon(
+                onPressed: () {
+                  // ( weight in kg / (height in metre)^2) = bmi
+
+                  var bmiTemp = _weightSliderValue / pow((_heightSliderValue / 100),2);
+
+                  // ONLY Assigning bmiTemp.round() into _bmi variable
+                 // _bmi = bmiTemp.round();
+
+                  // Assigning  bmiTemp.round() into _bmi variable
+                  // The widget to be refreshed
+
+                  var messageTemp = "";
+
+                  if (bmiTemp < 18){
+                    messageTemp = "You are underweight";
+                  }
+                  else if (bmiTemp < 25){
+                    messageTemp = "You are normal";
+                  }
+                  else if (bmiTemp < 30){
+                    messageTemp = "You are overweight";
+                  }
+                  else {
+                    messageTemp = "You are obese";
+                  }
+
+                  setState(() {
+                    // _bmi => int
+                    // bmiTemp => double
+                    // integer = double
+                    // in order to assign bmiTemp into _bmi, I have to transform it to int
+                    _bmi = bmiTemp.round();
+                    _message = messageTemp;
+                  });
+
+                },
+                icon: const Icon(Icons.favorite),
+                label: const Text("Calculate"))
+          ],
+        )),
+      ),
     );
   }
 }
